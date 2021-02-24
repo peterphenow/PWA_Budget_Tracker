876c726d532c10afa92129484da08b423b6f7946
Peter Phenow <peter.phenow@gmail.com>
2021-02-24 10:40:34 -0600

    Revert "switch double quote to single quote to make Travis happy"
    This reverts commit 4d06158b47033b51a538ea861da626ccea639e46.

--- server.js
+++ server.js
@@ -1,28 +1,28 @@
-const express = require('express');
-const logger = require('morgan');
-const mongoose = require('mongoose');
-const compression = require('compression');
+const express = require("express");
+const logger = require("morgan");
+const mongoose = require("mongoose");
+const compression = require("compression");
 
 const PORT = process.env.PORT || 3000;
 
 const app = express();
 
-app.use(logger('dev'));
+app.use(logger("dev"));
 
 app.use(compression());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-app.use(express.static('public'));
+app.use(express.static("public"));
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/budget', {
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/budget", {
   useNewUrlParser: true,
   useFindAndModify: false,
   useUnifiedTopology: true
 });
 
 // routes
-app.use(require('./routes/api.js'));
+app.use(require("./routes/api.js"));
 
 app.listen(PORT, () => {
   console.log(`App is listening at http://localhost:${PORT} !`);
