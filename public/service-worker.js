4d06158b47033b51a538ea861da626ccea639e46
Peter Phenow <peter.phenow@gmail.com>
2021-02-23 23:46:56 -0600

    switch double quote to single quote to make Travis happy

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,22 +1,22 @@
 const FILES_TO_CACHE = [
-  "/",
-  "/index.html",
-  "assets/js/index.js",
-  "assets/js/db.js",
-  "assets/css/styles.css",
-  "/manifest.json",
-  "https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css",
-  "https://cdn.jsdelivr.net/npm/chart.js@2.8.0"
+  '/',
+  '/index.html',
+  'assets/js/index.js',
+  'assets/js/db.js',
+  'assets/css/styles.css',
+  '/manifest.json',
+  'https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css',
+  'https://cdn.jsdelivr.net/npm/chart.js@2.8.0'
 ];
 
-const CACHE_NAME = "static-cache-v2";
-const DATA_CACHE_NAME = "data-cache-v1";
+const CACHE_NAME = 'static-cache-v2';
+const DATA_CACHE_NAME = 'data-cache-v1';
 
 // install
-self.addEventListener("install", function (evt) {
+self.addEventListener('install', function (evt) {
   evt.waitUntil(
     caches.open(CACHE_NAME).then((cache) => {
-      console.log("Your files were pre-cached successfully!");
+      console.log('Your files were pre-cached successfully!');
       return cache.addAll(FILES_TO_CACHE);
     })
   );
@@ -25,13 +25,13 @@ self.addEventListener("install", function (evt) {
 });
 
 // activate
-self.addEventListener("activate", function (evt) {
+self.addEventListener('activate', function (evt) {
   evt.waitUntil(
     caches.keys().then((keyList) => {
       return Promise.all(
         keyList.map((key) => {
           if (key !== CACHE_NAME && key !== DATA_CACHE_NAME) {
-            console.log("Removing old cache data", key);
+            console.log('Removing old cache data', key);
             return caches.delete(key);
           }
         })
@@ -43,9 +43,9 @@ self.addEventListener("activate", function (evt) {
 });
 
 // fetch
-self.addEventListener("fetch", function (evt) {
+self.addEventListener('fetch', function (evt) {
   const { url } = evt.request;
-  if (url.includes("/all") || url.includes("/find")) {
+  if (url.includes('/all') || url.includes('/find')) {
     evt.respondWith(
       caches
         .open(DATA_CACHE_NAME)
